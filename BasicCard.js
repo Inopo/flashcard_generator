0e89d119e82702a240f2a45bb1355365dac7a6b3
Ege <ertem@ephesusventures.com>
2017-04-08 06:00:03 -0700

    psudeo code for the soutline

---
+++ BasicCard.js
@@ -0,0 +1,2 @@
+//accept frontArguments
+//accept backArguments
---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+// Note: use scope safe constructors
+// Note: cosntructors should be written in a way that the user can call 
+// Note cnt: them with or without new word. 
+
+//accept textArguments
+//accept clozeArguments
+// method or property .cloze
+// method or property .partial
+// method or property .fullText
+// throw an err if cloze deletion can not be find in the txt.
+
+
+
