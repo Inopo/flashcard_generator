93d51318244f2964cb75b6697e4706c5b7b1252b
Ege <ertem@ephesusventures.com>
2017-04-16 03:49:05 +0300

    second

--- BasicCard.js
+++ BasicCard.js
@@ -1,4 +1,3 @@
-
 // Note: use scope safe constructors
 // Note: cosntructors should be written in a way that the user can call 
 // Note cnt: them with or without new word. 
@@ -6,18 +5,28 @@
 //there should be an obejct such as BasicCard
 
 var inquirer = require('inquirer');
-var BasicCard = function (string) {
+var BasicCard = function(front, back) {
 
-this.front = front;
-this.back = back;
+    this.front = front;
+    this.back = back;
 };
 
-//accept frontArguments
 
+//Empty array that will keep flashcards
+var arr = [];
 
+//Q&Az\s
+var material = {
+    "What was the name of the first electronic general-purpose computer": "ENIAC",
+    "What does HTTP stand for in a website address": "HyperText Transfer Protocol",
+    "In what year did Nintendo release its first game console in North America": "1985",
+    "In what year was the iPhone first released": "2007"
+};
 
-//accept backArguments
+//Loop through material and construct objects
+for (var key in material) {
+    arr.push(new BasicFlashcard(key, material[key]));
+}
 
-// .front 
-
-// .back
+//Export the array of objects and the constructor out
+module.exports = { arr, BasicFlashcard };
