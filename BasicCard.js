883a0ff77ab91c6997566eddbbc2673bc2c5bb3b
Ege <ertem@ephesusventures.com>
2017-04-16 03:21:16 +0300

    submit

--- BasicCard.js
+++ BasicCard.js
@@ -1,2 +1,23 @@
+
+// Note: use scope safe constructors
+// Note: cosntructors should be written in a way that the user can call 
+// Note cnt: them with or without new word. 
+
+//there should be an obejct such as BasicCard
+
+var inquirer = require('inquirer');
+var BasicCard = function (string) {
+
+this.front = front;
+this.back = back;
+};
+
 //accept frontArguments
+
+
+
 //accept backArguments
+
+// .front 
+
+// .back
