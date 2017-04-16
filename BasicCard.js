4f58711e0a41c4487ed4082e389f464f88b57101
Ege <ertem@ephesusventures.com>
2017-04-16 04:05:19 +0300

    final

--- BasicCard.js
+++ BasicCard.js
@@ -1,6 +1,5 @@
 // Note: use scope safe constructors
 // Note: cosntructors should be written in a way that the user can call 
-// Note cnt: them with or without new word. 
 
 //there should be an obejct such as BasicCard
 
@@ -17,16 +16,14 @@ var arr = [];
 
 //Q&Az\s
 var material = {
-    "What was the name of the first electronic general-purpose computer": "ENIAC",
-    "What does HTTP stand for in a website address": "HyperText Transfer Protocol",
-    "In what year did Nintendo release its first game console in North America": "1985",
-    "In what year was the iPhone first released": "2007"
+    "What is the name of best chess player ever": "Bobby Fischer",
+
 };
 
 //Loop through material and construct objects
 for (var key in material) {
-    arr.push(new BasicFlashcard(key, material[key]));
+    arr.push(new BasicCard(key, material[key]));
 }
 
 //Export the array of objects and the constructor out
-module.exports = { arr, BasicFlashcard };
+module.exports = { arr, BasicCard };
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,13 +1,28 @@
-// Note: use scope safe constructors
-// Note: cosntructors should be written in a way that the user can call 
-// Note cnt: them with or without new word. 
+//Constructor for the cloze flashcard
+var ClozeFlashcard = function(cloze, question) {
+    this.cloze = cloze;
+    this.question = question;
 
-//accept textArguments
-//accept clozeArguments
-// method or property .cloze
-// method or property .partial
-// method or property .fullText
-// throw an err if cloze deletion can not be find in the txt.
+    this.displayQuestion = function() {
+        var display = question.replace(cloze, "...");
+        return display;
+    }
 
+}
 
+//Empty array that will house our cloze flashcard objects
+var arr = [];
 
+//Key Value pairs for the cloze object
+var material = {
+    "1973": "Ege's birthday",
+
+};
+
+//Loop through material and construct objects
+for (var key in material) {
+    arr.push(new ClozeFlashcard(key, material[key]));
+}
+
+//Export the array of objects and the constructor 
+module.exports = { arr, ClozeFlashcard };
